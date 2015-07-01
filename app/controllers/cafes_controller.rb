f8000c483ec0b6410b6c5dbbb492ceb481fdbc8a
Lindsey <lameyer@users.noreply.github.com>
2015-07-01 00:50:08 -0700

    add creating new cafes

--- app/controllers/cafes_controller.rb
+++ app/controllers/cafes_controller.rb
@@ -8,4 +8,19 @@ class CafesController < ApplicationController
     @cafe = Cafe.find(params[:id])
   end
 
+  def new
+    @cafe = Cafe.new
+  end
+
+  def create
+    @cafe = Cafe.create(cafe_params)
+    redirect_to @cafe
+  end
+
+  private
+
+  def cafe_params
+    params.require(:cafe).permit(:name)
+  end
+
 end
