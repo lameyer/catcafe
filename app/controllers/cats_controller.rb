8725109f96bfd10d03fc37a17889b0598f07136b
Lindsey <lameyer@users.noreply.github.com>
2015-07-01 00:54:58 -0700

    add creating new cats

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -3,4 +3,20 @@ class CatsController < ApplicationController
   def show
     @cat = Cat.find(params[:id])
   end
+
+  def new
+    @cat = Cat.new
+  end
+
+  def create
+    @cat = Cat.create(cat_params)
+    redirect_to @cat
+  end
+
+  private
+
+  def cat_params
+    params.require(:cat).permit(:name, :age, :gender, :breed, :color, :personality)
+  end
+
 end
