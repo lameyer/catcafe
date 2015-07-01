0dcf950fde4823d88435cd8b788f0e955f82b4cd
Lindsey <lameyer@users.noreply.github.com>
2015-07-01 00:03:10 -0700

    make visited_cats unique

--- app/models/cafe.rb
+++ app/models/cafe.rb
@@ -1,5 +1,5 @@
 class Cafe < ActiveRecord::Base
   has_many :visits
-  has_many :lifetime_cats, through: :visits, source: :cat
+  has_many :visited_cats, -> { uniq }, through: :visits, source: :cat
   has_many :current_cats, -> { where("visits.exited_at"=> nil) }, through: :visits, source: :cat
 end
--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,19 +1,21 @@
 class Cat < ActiveRecord::Base
   has_many :visits
-  has_many :lifetime_cafes, through: :visits, source: :cafe
+  has_many :visited_cafes, -> { uniq }, through: :visits, source: :cafe
 
   def current_cafe
-    lifetime_cafes.where("visits.exited_at"=> nil).first
+    current_visit.cafe if current_visit
   end
 
   def current_visit
     visits.where("visits.exited_at"=> nil).first
   end
 
+  def visits_to_cafe(cafe)
+    visits.where("visits.cafe_id"=> cafe)
+  end
+
   def leave_current_cafe!
-    if current_visit
-      current_visit.update(exited_at: Time.now)
-    end
+    current_visit.update(exited_at: Time.now) if current_visit
   end
 
   def switch_cafe!(cafe)
@@ -21,8 +23,6 @@ class Cat < ActiveRecord::Base
     visit = Visit.create(cat: self, cafe: cafe, entered_at: Time.now)
   end
 
-  def visits_to_cafe(cafe)
-    visits.where("visits.cafe_id"=> cafe)
   end
 
 end
