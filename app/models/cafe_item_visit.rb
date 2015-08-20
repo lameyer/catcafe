5ab34c82af4064e4b252f3e8c50197b072081512
Lindsey <lameyer@users.noreply.github.com>
2015-08-19 21:41:05 -0700

    no belongs_to through?!?!

--- app/models/cafe_item_visit.rb
+++ app/models/cafe_item_visit.rb
@@ -2,8 +2,8 @@ class CafeItemVisit < ActiveRecord::Base
   belongs_to :cat
   belongs_to :cafe_item
   belongs_to :cafe_visit
-  belongs_to :cafe, through: :cafe_item, source: :cafe
-  belongs_to :item, through: :cafe_item, source: :item
+  has_one :cafe, through: :cafe_item, source: :cafe
+  has_one :item, through: :cafe_item, source: :item
 
   def elapsed_duration
     if self.exited_at
