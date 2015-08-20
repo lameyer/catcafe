b0f093953c96e17948b61ca3312ef1234b3923dc
Lindsey <lameyer@users.noreply.github.com>
2015-08-19 22:21:14 -0700

    Make cat leave cafe when you delete its cafe_item

--- app/models/cafe_item.rb
+++ app/models/cafe_item.rb
@@ -1,10 +1,20 @@
 class CafeItem < ActiveRecord::Base
   belongs_to :item
   belongs_to :cafe
-  has_many :cafe_item_visits
+  has_many :cafe_item_visits, dependent: :destroy
+
+  before_destroy :make_cat_leave_cafe, prepend: true
 
   def current_visit
     cafe_item_visits.where(:exited_at => nil).first
+  end
+
+  def current_cat
+    current_visit.cat if current_visit
+  end
+
+  def make_cat_leave_cafe
+    current_cat.leave_current_cafe! if current_cat
   end
 
 end
